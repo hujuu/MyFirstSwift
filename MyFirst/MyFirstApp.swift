f2fad0b926dd742e3562d58954b7612c04b70078
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-11 22:08:59 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by KEN NAKAI on 2023/11/11.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
